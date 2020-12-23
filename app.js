5471da7b806de6454588ad1239ca379b190af235
lina-yousef <lina.aa96@hotmail.com>
2020-12-23 23:23:29 +0200

    the new update for lab06

---
+++ app.js
@@ -0,0 +1,17 @@
+var userName =prompt('Please enter your name');
+alert('Hi '+userName+'! Welcome to Pink Flower');
+
+document.write('Hi'+' '+userName+'! Welcome to Pink Flower store');
+
+var budget = 50 ;
+            var message;
+          
+            if (budget < 50) {
+                message='Order now and get 30% discount for shipping'+userName;
+
+            }
+            else {
+                message='Order now and get a free shipping'+userName;
+            }
+            var budget = prompt('insert your budget');
+
