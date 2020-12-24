39a2211833d3f94a4915141190ffcf3888f3b63a
lina-yousef <lina.aa96@hotmail.com>
2020-12-24 16:59:54 +0200

    lab08

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var userName = prompt('Please enter your name');
+
+while (userName === null || userName === '' ) {
+    userName = prompt('Please enter your name');
+}
 console.log(userName);
+
 alert('Hi ' + userName + '! Welcome to Pink Flower');
 
-document.write('Hi' + ' ' + userName + '! Welcome to Pink Flower store');
+document.write('Hi ' + userName + '! Welcome to Pink Flower store');
 
 var budget = 50;
 var message;
@@ -10,12 +15,24 @@ budget = prompt('insert your budget');
 
 if (budget < 50) {
     message = 'Order now and get 30% discount for shipping' + userName;
-
 }
 else {
-    message = 'Order now and get a free shipping ,' + + userName;
+    message = 'Order now and get a free shipping ,' + userName;
 }
 
-
 alert(message);
+
+
+
+var Bouquet = prompt('How many Bouquet do you want ?');
+var tagToAdd = "";
+for (var i= 0;  Bouquet > i ; i++) {
+
+    //tagToAdd = tagToAdd + '<p>' + "" + Bouquet+'</p>';
+    document.write('<p>'+(i+1)+'</p>');
+    document.write('<img src="flower.webp">');
+
+   //var  document.getElementById()
+
+}
 
